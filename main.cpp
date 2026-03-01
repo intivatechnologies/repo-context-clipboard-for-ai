73fe8dba0aac198361ef7cac701f70ce8b7c424d
mikesinclair66 <michaelkevin.sinclair@gmail.com>
2026-02-28 16:40:04 -0800

    Finished f2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <stack>
 #include <filesystem>
+#include <algorithm>
 
 using namespace std;
 
@@ -25,26 +26,55 @@ public:
 		: line(line), placeOperation(placeOperation) {}
 };
 
+string getRootName(filesystem::path path) {
+	return path.string().substr(path.string().find_last_of("\\") + 1, path.string().size() - 1);
+}
+
+static const vector<string> ignoreNames({ ".git", "build" });
+
+vector<PlacedLine> getPlacedLines(string root) {
+	PlaceOperation nextOperation = PlaceOperation::TABULATE;
+	string rootName = getRootName(root);
+	vector<PlacedLine> lines;
+
+	//examines each name in the directory
+	for (const auto& entry : filesystem::directory_iterator(root)) {
+		//extract the file/folder name
+		string eps = entry.path().string(), name = eps.substr(root.size() + 1, eps.size() - 1);
+
+		if (entry.is_regular_file())
+			lines.push_back(PlacedLine(name + ",", nextOperation));
+		else {
+			auto ignoreNameCheck = find(ignoreNames.begin(), ignoreNames.end(), name);
+
+			if (ignoreNameCheck != ignoreNames.end())
+				lines.push_back(PlacedLine(name + ": ... ,", nextOperation));
+			else {
+				lines.push_back(PlacedLine(name + ": {", nextOperation));
+
+				vector<PlacedLine> nestedLines = getPlacedLines(root + "\\" + name);
+				for (const auto& nestedEntry : nestedLines)
+					lines.push_back(nestedEntry);
+
+				lines.push_back(PlacedLine("}", nestedLines.size() > 0
+					? PlaceOperation::DETABULATE : PlaceOperation::CONTINUE));
+			}
+		}
+
+		if (nextOperation != PlaceOperation::CONTINUE)
+			nextOperation = PlaceOperation::CONTINUE;
+	}
+
+	return lines;
+}
+
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
-		PlaceOperation nextOperation = PlaceOperation::TABULATE;
-		string root = argv[1], rootName = root.substr(root.find_last_of("\\") + 1, root.size() - 1);
-		vector<PlacedLine> lines({ PlacedLine(rootName + ": {")});
+		vector<PlacedLine> lines({ PlacedLine(getRootName(argv[1]) + ": {")});
+		vector<PlacedLine> nestedLines = getPlacedLines(argv[1]);
 
-		//examines each name in the directory
-		for (const auto& entry : filesystem::directory_iterator(argv[1])) {
-			string eps = entry.path().string(), name = eps.substr(root.size() + 1, eps.size() - 1);
-			if(entry.is_regular_file())
-				lines.push_back(PlacedLine(name, nextOperation));
-			else {
-				/* CHANGE THIS */
-				lines.push_back(PlacedLine(name + ": {", nextOperation));
-				lines.push_back(PlacedLine("}"));
-			}
-
-			if (nextOperation != PlaceOperation::CONTINUE)
-				nextOperation = PlaceOperation::CONTINUE;
-		}
+		for (const auto& nestedEntry : nestedLines)
+			lines.push_back(nestedEntry);
 		lines.push_back(PlacedLine("}", PlaceOperation::DETABULATE));
 
 		string indentation = "";
